25d512396318f9e02217f6e0762c000f23e90fce
Parvez-hasan <parvez7788@gmail.com>
2025-08-15 00:15:55 +0600

    solution to problem 3 and 4

--- problem-4.js
+++ problem-4.js
@@ -2,5 +2,28 @@
 /** solution to problem-4 **/
 
 function  isSame(arr1 , arr2 ) {
-          
-}
+          if(Array.isArray(arr1) !== true || Array.isArray(arr2) !== true){
+
+            return "Invalid";
+          };
+
+          if(arr1.length !== arr2.length){
+
+            return false
+          };
+
+        for(let i = 0; i < arr1.length; i++){
+
+            if(arr1[i] !== arr2[i]){
+
+                return false
+            }
+        }
+        return true
+};
+
+console.log(isSame([1, 2, 3] , [1 , 2 , 3]));
+console.log(isSame([34 , 5 ,7 ,9 ], [ 34 , 5 , 7 ]));
+console.log(isSame([1, undefined , 3] , [1,null ,3]));
+console.log(isSame({data: [2 , 5 , 6] } , [2 , 5 , 6]));
+
